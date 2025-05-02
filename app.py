45750b3e750173d6db605372617f3775663ae516
tantinoroj <tan.t@chevron.com>
2025-05-02 20:45:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route(‘/greet’, methods=[‘POST’])
+def greet():
+  name = request.form[‘name’]
+  return f’Hello, {name}!’
 @app.route("/")
 def hello():
     return render_template('index.html', message='Hello, World!')
